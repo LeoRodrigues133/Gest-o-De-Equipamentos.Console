5363c2ae7e7b8b95685751f516dec3d8a473b9ae
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-04-16 18:26:33 -0300

    Commit to delivery Before UX update

---
+++ src/GestaoEquipamentos.ConsoleApp/Produto.cs
@@ -0,0 +1,26 @@
+﻿namespace GestaoEquipamentos.ConsoleApp
+{
+    public class Produto
+    {
+        public string nome, numeroDeSerie, fabricante;
+        public string dataDeFabricacao;
+        public double preco;
+        public int ID = novoID; // Já inicia em 1;
+        private static int novoID;
+        public Produto(string numeroDeSerie, int ID, string nome, string dataDeFabricacao, string fabricante, double preco)
+        {
+            this.numeroDeSerie = numeroDeSerie;
+            this.ID = GerarIdDoProduto();
+            this.nome = nome;
+            this.dataDeFabricacao = dataDeFabricacao;
+            this.fabricante = fabricante;
+            this.preco = preco;
+        }
+
+        public static int GerarIdDoProduto()
+        {
+            //Obrigado Thiago, eu tava me batendo nisso; <3
+            return novoID++;
+        }
+    }
+}
