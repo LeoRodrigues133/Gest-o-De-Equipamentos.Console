d539816d0f4526db949904d874559cbb4e9ac2ed
Leonardo Rodrigues <lthkrieger@gmail.com>
2024-04-19 11:34:37 -0300

    Added Chamado && RepositórioChamados;

---
+++ src/GestaoEquipamentos.ConsoleApp/ModuloChamado/Chamado.cs
@@ -0,0 +1,26 @@
+﻿//namespace GestaoEquipamentos.ConsoleApp.ModuloChamados
+//{
+//    public class Chamado
+//    {
+//        public string nome, descricao, dataDoChamado;
+//        public double valorChamado;
+//        public int idChamado = novoIDChamados;
+//        public static int novoIDChamados;
+
+
+//        public Chamado(int idChamados, string nome, string descricao, string dataDoChamado, double valorChamado)
+//        {
+//            this.idChamado = GerarIdChamados();
+//            this.nome = nome;
+//            this.descricao = descricao;
+//            this.dataDoChamado = dataDoChamado;
+//            this.valorChamado = valorChamado;
+//        }
+
+//        public static int GerarIdChamados()
+//        {
+
+//            return novoIDChamados++;
+//        }
+//    }
+//}
---
+++ src/GestaoEquipamentos.ConsoleApp/ModuloChamado/RepositorioChamados.cs
@@ -0,0 +1,44 @@
+﻿//using GestaoEquipamentos.ConsoleApp.ModuloEquipamentos;
+
+//namespace GestaoEquipamentos.ConsoleApp.ModuloChamados
+//{
+//    public class RepositorioChamados
+//    {
+//        public List<Chamado> suporte = new List<Chamado>();
+//        public int idChamado = Chamado.GerarIdChamados();
+
+//        //public Chamado(int idChamados, string nome, string descricao, string dataDoChamado, double valorChamado)
+
+//        public void Create() {
+//            string nome = Program.ObterValor<string>("Nome do Chamado: ");
+//            string descricao = Program.ObterValor<string>("Descrição do chamado: ");
+//            string dataDoChamado = Program.ObterValor<string>("Data de criação do chamado: ");
+//            double valorChamado = Program.ObterValor<double>("Valor do chamado: ");
+
+//                Chamado novoChamado = new Chamado(idChamado, nome, descricao, dataDoChamado, valorChamado);
+//        }
+//        private void AdicionarChamado(Chamado chamado)
+//        {
+//            suporte.Add(chamado);
+//        }
+
+//        public void Read() {
+
+//            Menu.CabecalhoChamado();
+//            foreach( var chamado in suporte)
+//            {
+
+//                Console.WriteLine($"|Id: {chamado.idChamado}".PadRight(7) +
+//                                  $"|Nome: {chamado.nome}".PadRight(23) +
+//                                  $"|Descrição: {chamado.descricao}".PadRight(19) +
+//                                  $"|Data: {chamado.dataDoChamado}".PadRight(26) +
+//                                  $"|Valor R$ {chamado.valorChamado:F}".PadRight(21) + " |");
+//            }
+//            Menu.Rodape();
+//        }
+//        public void Update() { }
+//        public void Delete() { }
+
+
+//    }
+//}
